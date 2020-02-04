7f7d64c216a5c41a113ab159adb9b5e38be2a6f2
Michal <fajnykapelusz@gmail.com>
2020-02-04 18:57:24 +0100

    Updates

---
+++ precache-manifest.4227a77a25cea62bcd9179f420f1cec6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8159525807f5463d484be7978b82d251",
+    "url": "/portfoliov2/index.html"
+  },
+  {
+    "revision": "38806561ea3ac2853573",
+    "url": "/portfoliov2/static/css/main.3180bdec.chunk.css"
+  },
+  {
+    "revision": "513de246cf53488d23aa",
+    "url": "/portfoliov2/static/js/2.23a1ef11.chunk.js"
+  },
+  {
+    "revision": "38806561ea3ac2853573",
+    "url": "/portfoliov2/static/js/main.0ec71012.chunk.js"
+  },
+  {
+    "revision": "ae5cfbb44cc57307ca59",
+    "url": "/portfoliov2/static/js/runtime-main.ba8bf48a.js"
+  }
+]);
